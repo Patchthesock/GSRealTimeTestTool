c1bc2494b77787937459c15fd8603369a6d274ae
Patchthesock <stephencallaghan91@gmail.com>
2018-07-09 10:13:26 +0100

    Added unit tests

---
+++ Assets/Tests/Editor/Models/TestLatency.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Models;
+using NUnit.Framework;
+using Zenject;
+
+namespace Tests.Editor.Models
+{
+    [TestFixture]
+    public class TestLatency : ZenjectUnitTestFixture
+    {
+        [Test]
+        public void TestLagGivenZero()
+        {
+            var l = new Latency(0, 0);
+            Assert.AreEqual(l.Lag, 0);
+        }
+
+        [Test]
+        public void TestRoundTripGivenZero()
+        {
+            var l = new Latency(0, 0);
+            Assert.AreEqual(l.RoundTrip, 0);
+        }
+        
+        [Test]
+        public void TestThroughputGivenZero()
+        {
+            var l = new Latency(0, 0);
+            Assert.AreEqual(l.Throughput, 0);
+        }
+
+        [Test]
+        public void TestLagGiven100MsOffset()
+        {
+            const int ping = 1;
+            const int pong = ping + 100;
+            
+            var l = new Latency(ping, pong);
+            Assert.AreEqual(l.Lag, pong - ping);
+        }
+    }
+}
---
+++ Assets/Tests/Editor/Services/TestRtQosService.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using Services;
+using Zenject;
+
+namespace Tests.Editor.Services
+{
+	[TestFixture]
+	public class TestRtQosService : ZenjectUnitTestFixture
+	{
+		[SetUp]
+		public void CommonInstall()
+		{
+			//Container.Bind<AsyncProcessor>().AsSingle();
+			//Container.Bind<RtQosService>().AsSingle();
+		}
+		
+		[Test]
+		public void CanStartPingTestTest()
+		{
+			//var s = Container.Resolve<RtQosService>();
+		}
+
+
+	}
+}
